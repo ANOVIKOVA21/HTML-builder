5585f07caddf0a5f21990403296b9d9d9bd1ff7b
ANOVIKOVA21 <anna2013novikova@gmail.com>
2021-11-01 14:35:47 +0200

    feat: add function for reading file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const way = path.join(__dirname, 'text.txt')
+const stream = fs.createReadStream(way, 'utf-8');
+
+let data = '';
+
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
