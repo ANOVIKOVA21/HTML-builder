703ad49f0e48c781225aa41442c6700b06d4bb01
ANOVIKOVA21 <anna2013novikova@gmail.com>
2021-11-01 17:32:41 +0200

    feat: add function create and add text in new file

---
+++ 02-write-file/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+const way = path.join(__dirname, 'text.txt')
+const output = fs.createWriteStream(way);
+
+stdout.write('Enter some text\n');
+stdin.on('data', data => {
+    const str = data.toString();
+    if (str.trim() === 'exit') {
+        stdout.write('Goodbye');
+        process.exit();
+    }
+    output.write(str)
+});
+process.on('SIGINT', () => {
+    console.log('\nGoodbye');
+    process.exit();
+})
