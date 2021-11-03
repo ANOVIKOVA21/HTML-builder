b321bb86804fde41fa5b3af6aeccee67a1356742
ANOVIKOVA21 <anna2013novikova@gmail.com>
2021-11-03 14:54:27 +0200

    feat: add function copyDir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+    const way = path.join(__dirname, 'files-copy')
+    const src = path.join(__dirname, 'files')
+    fs.promises.mkdir(way, { recursive: true }).then(function() {
+        fs.readdir(src, (err, files) => {
+            if (err) throw err;
+            files.forEach(file => {
+                fs.copyFile(src + '/' + file, way + '/' + file, (err) => {
+                    if (err) throw err;
+                })
+            });
+        });
+        console.log('Directory created successfully');
+    })
+}
+copyDir()
